016fba83228580bb154943cb4b993480afb63262
sunss <sss213018@163.com>
2020-08-16 00:49:09 +0800

    edit deploy.js

--- deploy.js
+++ deploy.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+const express = require("express")
+const cors = require("cors")
+const fs = require("fs")
+const path = require("path")
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, "./public")))
 
-app.get('*', function(req, res) {
-    const html = fs.readFileSync(path.resolve(__dirname, './public/index.html'), 'utf-8');
-    res.send(html);
-});
+app.get("*", function (req, res) {
+    const html = fs.readFileSync(
+        path.resolve(__dirname, "./docs/.vuepress/dist/index.html"),
+        "utf-8"
+    )
+    res.send(html)
+})
 
-app.listen(4001);
+app.listen(4001)
