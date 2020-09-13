5fc4cafd881801aa8a9429e5d4c90b462edb7793
sunss <sss213018@163.com>
2020-09-13 17:09:28 +0800

    delete License

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=http://www.ssscode.com&token=0o9VsNZfkMJUz9Gg"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.ssscode.com&token=0o9VsNZfkMJUz9Gg"
 
 rm -rf urls.txt # 灭迹
