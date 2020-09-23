bfc73d13d3ba1fe868054165127e808903726b74
sunss <sss213018@163.com>
2020-09-24 00:03:04 +0800

    👌  压缩图片 ，添加网站加速文章

--- theme-vdoing/components/Footer.vue
+++ theme-vdoing/components/Footer.vue
@@ -15,24 +15,29 @@
     <!-- Theme by <a href="https://github.com/xugaoyi/vuepress-theme-vdoing" target="_blank" title="本站主题">Vdoing</a> -->
     <span>{{ footer.copyright }}</span>
     <template v-if="footer">
-      | © {{ footer.createYear }}-{{
-        new Date().getFullYear() + "." + (new Date().getMonth() + 1)
-      }}
+      | © {{ footer.createYear }}-{{ new Date().getFullYear() + '.' + (new Date().getMonth() + 1) }}
       <span v-html="footer.copyrightInfo"></span>
     </template>
+    <div class="cloud">
+      本网站由
+      <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral">
+        <img src="/img/upyun.png" alt="" />
+      </a>
+      提供CDN加速/云存储服务
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    social() {
-      return this.$themeConfig.social;
-    },
-    footer() {
-      return this.$themeConfig.footer;
+    computed: {
+        social () {
+            return this.$themeConfig.social;
+        },
+        footer () {
+            return this.$themeConfig.footer;
+        }
     }
-  }
 };
 </script>
 
@@ -45,6 +50,19 @@ export default {
     box-sizing: border-box;
     font-size: 0.85rem;
     transition: all 0.2s ease;
+
+    .cloud {
+        margin-top: 5px;
+        font-size: 12px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            width: 2.9rem;
+            vertical-align: middle;
+        }
+    }
 
     .icons {
         margin-bottom: 12px;
