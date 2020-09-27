21b003d690e019b96629fa3d91a5a841243c53ff
sunss <sunss@robooot.com>
2020-09-27 21:51:38 +0800

    🔧 edit ssh ip

--- coding.sh
+++ coding.sh
@@ -1,7 +1,7 @@
 # !/usr/bin/env sh
 
 # 直接通过上传替换源文件进行更新
-# scp -r ./docs/.vuepress/dist root@ssscode.com:/usr/local/app/vuepress-blog/
+# scp -r ./docs/.vuepress/dist root@101.200.56.164:/usr/local/app/vuepress-blog/
 
 # 同步到coding
 echo '开始push到 coding =======start'
