3fc313f29d601af6a3e46187f57df492e751f797
sunss <sss213018@163.com>
2020-11-15 12:13:45 +0800

    🔧 优秀技术文章=>调整为每一项分类为单个文件

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -44,7 +44,7 @@ module.exports = [
       { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
       { text: '资源', link: '/pages/eee83a9211a70f9d/' },
       { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-      { text: '优秀技术文章', link: '/pages/f9569f/' },
+      { text: '优秀技术文章', link: '/pages/0d3db5/' },
     ],
   },
   {
