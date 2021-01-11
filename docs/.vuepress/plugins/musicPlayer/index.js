1cdb2f948f9f154601c43cfac399d3d5b687ae1b
sunss <sss213018@163.com>
2021-01-11 23:03:19 +0800

    ✨ 引入播放器插件

---
+++ docs/.vuepress/plugins/musicPlayer/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+// global.navigator = {
+//     userAgent: 'node.js'
+// };
+module.exports = (options, context) => ({
+  define() {
+    // const { icon, detailsStyle, visibilityHeight } = options
+    return {
+      // ICON: icon || 'reco-up',
+      // DETAILS_STYLE: detailsStyle || {
+      //   right: '1rem',
+      //   bottom: '9rem',
+      //   width: '2.5rem',
+      //   height: '2.5rem',
+      //   'border-radius': '.25rem',
+      //   'line-height': '2.5rem',
+      //   //backgroundColor: 'rgba(231, 234, 241,.5)',
+      //   'font-size': '14px',
+      //   'font-weight': '500'
+      // },
+      // VISIBILITY_HEIGHT: visibilityHeight || 400
+    };
+  },
+  name: 'vuepress-plugin-musicPlayer',
+  enhanceAppFiles: [path.resolve(__dirname, './bin/enhanceAppFile.js')],
+  globalUIComponents: 'musicPlayer',
+});
