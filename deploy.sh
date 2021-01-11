0caee5a984850a2621d2a352de3110affd464e76
sunss <sss@dataqin.com>
2021-01-11 15:22:24 +0800

    ✅ 测试：coding 部署地址调整 test 4

--- deploy.sh
+++ deploy.sh
@@ -28,8 +28,12 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # deploy to coding
 echo 'www.ssscode.com\ssscode.com' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  echo "111" 
+  echo "$CODING_TOKEN" 
   codingUrl=git@e.coding.net:ssscode/vuepress.git
 else
+  echo "222" 
+  echo "$CODING_TOKEN"
   codingUrl=https://asdJimNXfh:${CODING_TOKEN}@e.coding.net/ssscode/vuepress.git #注意！！！这里需要使用coding提供的个人令牌的用户名和token
 fi
 git add -A
