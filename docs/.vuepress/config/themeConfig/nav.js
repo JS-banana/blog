5c5467bf87d60795500a27563d9acce763fe7fcc
JS-banana <sss213018@163.com>
2024-03-26 10:57:44 +0800

    refactor: 目录结构重构

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,43 +1,29 @@
 module.exports = [
   { text: '首页', link: '/' },
-  // {
-  //   text: '前端',
-  //   link: '/web/', //目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-  //   items: [
-  //     { text: 'JavaScript', link: '/pages/627866/' },
-  //     { text: '面试', link: '/pages/b98f79/' },
-  //   ],
-  // },
   {
-    text: '文章',
-    link: '/post/',
-    items: [
-      { text: '前端', link: '/pages/911929/' },
-      { text: '服务', link: '/pages/337720/' },
-      { text: 'Node', link: '/pages/6e5f36/' },
-    ],
-  },
+    text: '前端',
+    link: '/frontend/' ,
+    items:[
+      { text: '基础', link: '/pages/be6faa/' },
+      { text: '框架', link: '/pages/33f292/' },
+      { text: '进阶', link: '/pages/c8aad6/' },
+      { text: '工程化', link: '/pages/2833ad/' },
+    ]
+  }, //frontend
   {
-    text: '更多',
-    link: '/more/',
-    items: [
+    text: '全栈',
+    link: '/fullstack/',
+    items:[
+      { text: 'NodeJS', link: '/pages/5fe09e/' },
+      { text: '脚本', link: '/pages/337720/' },
+      { text: '拓展', link: '/pages/131a7a/' },
       { text: '技术', link: '/pages/04df1e/' },
-      { text: '服务器', link: '/pages/87a1a5/' },
-      { text: '程序猿', link: '/life/' },
-    ],
-  },
+      { text: '服务器', link: '/pages/5cd35f/' },
+    ]
+  }, //fullstack
+  { text: '程序猿', link: '/life/' },
   { text: '关于', link: '/about/' }, //about
   { text: '友链', link: '/friends/' }, // friends
-  {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    items: [
-      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-      { text: '优秀技术文章', link: '/pages/0d3db5/' },
-    ],
-  },
   {
     text: '索引',
     link: '/archives/',
